c4df1307c9db7306c8373d7742f7dcbde360cf02
eduvf <>
2024-06-20 14:52:44 +0200

    add no newline option

--- extract_code.py
+++ extract_code.py
@@ -3,6 +3,9 @@
 import argparse
 
 parser = argparse.ArgumentParser()
+parser.add_argument('-n', '--no-newline',
+    action='store_true',
+    help='disable extra newline characters between code blocks')
 parser.add_argument('file')
 
 args = parser.parse_args()
@@ -12,6 +15,8 @@ with open(args.file, 'r') as f:
     
     for line in f:
         if line.startswith('```'):
+            if code and not args.no_newline:
+                print()
             code = not code
         elif code:
             print(line.rstrip())
